93fba2c895738fecc60f41168f86145f7e9b3d92
tomas-jimenez_gmxt <203015435+TomFXE@users.noreply.github.com>
2025-10-23 09:22:17 -0600

    Sprint 7: cambio de url de imagenes

---
+++ los-robles-residencial/src/mocks/modelsData.js
@@ -0,0 +1,26 @@
+// src/mocks/modelsData.js (Nuevo Archivo)
+
+// Datos extraídos de ModelsPage.jsx [cite: 162-188]
+export const modelsData = [
+  {
+    id: 1,
+    name: 'Modelo Fresno',
+    sqMeters: 180,
+    price: '$3,500,000',
+    imgUrl: 'https://tse4.mm.bing.net/th/id/OIP.daFFq0kuQAwfitAR0doLFwHaFi?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3'
+  },
+  {
+    id: 2,
+    name: 'Modelo Roble',
+    sqMeters: 220,
+    price: '$4,200,000',
+    imgUrl: 'https://images.homify.com/image/upload/a_0/v1437365675/p/photo/image/753966/CHAPALA_0.jpg'
+  },
+  {
+    id: 3,
+    name: 'Modelo Cedro',
+    sqMeters: 250,
+    price: '$4,800,000', // (Precio corregido del PDF [cite: 187])
+    imgUrl: 'https://th.bing.com/th/id/R.fd613f431ab49388890b145d512f7213?rik=ZyRgcHKWgZ8rqQ&riu=http%3a%2f%2finmuebles.moncavil.com%2fwp-content%2fuploads%2f2019%2f06%2fCASA-FRACC.jpg&ehk=kXmpylBql%2bk7bgxF3T4teCEYYyA0PafaJAXvqWwTVp8%3d&risl=&pid=ImgRaw&r=0'
+  }
+];
